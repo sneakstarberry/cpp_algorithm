ac5c1759aae47d048823e2e27e40506a3b4f4697
sneakstarberry <gihong2012@likelion.org>
2020-05-17 23:30:40 +0900

    first commit

---
+++ q4344/q4344.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int c, n , x, cnt, num;
+    vector<int> vec;
+    cin >> c;
+
+    
+    for(int i=0; i<c;i++){
+        vec.clear();
+        n=0;
+        num = 0;
+        cnt = 0;
+        cin >>n;
+        for(int j=0; j< n;j++){
+            x=0;
+            cin>>x;
+            vec.push_back(x);
+            num+=vec[j];
+        }
+        int aver = num/n;
+        for (vector<int>::size_type j=0; j< vec.size(); j++){
+            if (aver < vec[j]){
+                cnt++;
+            }
+        }
+
+        cout.precision(3);
+        cout << fixed <<(double)cnt/n*100 << "%" << endl;
+    }
+    return 0;
+}
